574d6fe02858455317e238f7b044d0e8fe5a4b87
malachornix <malachornix@gmail.com>
2018-10-15 22:55:07 +0400

    734A

---
+++ src/Problem734A.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Problem734A {
+
+    public static void main(String[] args) {
+        char[] results = InputResults();
+        int n = results.length;
+        int antonWinTimes = 0;
+        for (char result : results) {
+            if (result == 'A') {
+                antonWinTimes++;
+            }
+        }
+        if (antonWinTimes > n - antonWinTimes) {
+            System.out.println("Anton");
+        } else if (antonWinTimes < n - antonWinTimes) {
+            System.out.println("Danik");
+        } else {
+            System.out.println("Friendship");
+        }
+    }
+
+    private static char[] InputResults() {
+        Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter(System.lineSeparator());
+        Integer n = Integer.valueOf(scanner.next());
+        char[] results = scanner.next().toCharArray();
+        scanner.close();
+        return results;
+    }
+}
